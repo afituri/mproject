64e393ec439521be80d2f1dce32fc7712eadc0cb
FaisalRahil <faisallrahil@gmail.com>
2015-02-25 11:34:59 +0200

    edit

--- public/js/testPage.js
+++ public/js/testPage.js
@@ -1,15 +1,21 @@
 $(document).ready(function(){
-  $("#input-702").fileinput({
-    uploadUrl: "' . Url::to(['/site/file-upload']) . '"
-    maxFileCount: 10,
-    overwriteInitial: false,
-    initialPreview: [
-    '<img src="http://www.kodyaz.com/photos/windows_8/images/34677/secondarythumb.aspx " class="file-preview-image">',
-    '<img src="http://www.kodyaz.com/photos/windows_8/images/34676/secondarythumb.aspx " class="file-preview-image">',
-    ],
-    initialPreviewConfig: [
-    {caption: "Desert.jpg", width: "120px", url:"/site/file-delete", key:1},
-    {caption: "Tulips.jpg", width: "120px", url:"/site/file-delete", key:2}
-    ],
-  });
+    alert("Inside function hhhhhhhhhhhhh");
+    $('.navmenu').offcanvas();
+    // $('.navmenu').canvas-slid();
+   
+
+    $("#input-702").fileinput({
+        uploadUrl: "' . Url::to(['#']) . '"
+        maxFileCount: 10,
+        overwriteInitial: false,
+        initialPreview: [
+            '<img src="http://www.kodyaz.com/photos/windows_8/images/34677/secondarythumb.aspx " class="file-preview-image">',
+            '<img src="http://www.kodyaz.com/photos/windows_8/images/34676/secondarythumb.aspx " class="file-preview-image">',
+        ],
+        initialPreviewConfig: [
+        {caption: "Desert.jpg", width: "120px", url:"/site/file-delete", key:1},
+        {caption: "Tulips.jpg", width: "120px", url:"/site/file-delete", key:2}
+        ],
+    });
+
 });
--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,9 @@ router.get('/testPage', function(req, res) {
   res.render('testPage', { title: 'testPage' });
 });
 
+/* GET testTwo. */
+router.get('/testTwo', function(req, res) {
+  res.render('testTwo', { title: 'testTwo' });
+});
+
 module.exports = router;
